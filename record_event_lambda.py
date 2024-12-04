ec45bb7ce419a4a41c9b5ab4a9887597c51c840d
maalekj <maalekj@gmail.com>
2024-12-04 23:35:07 +0300

    the location now consist of latitude and longitude

--- record_event_lambda.py
+++ record_event_lambda.py
@@ -21,6 +21,12 @@ def lambda_handler(event, context):
         # Base64 encode the image
         image = body.get("image")
 
+        # Extract event type, latitude, longitude, and timestamp from the request body
+        event_type = body.get("event_type")
+        latitude = Decimal(str(body.get("latitude")))
+        longitude = Decimal(str(body.get("longitude")))
+        event_timestamp = body.get("event_timestamp")
+
         # Call the event_validate_lambda function
         validation_response = call_event_validate_lambda(image)
 
@@ -37,14 +43,22 @@ def lambda_handler(event, context):
         print("Validation result:", validation_result)
 
         # Check the validation response
-        if not validation_result["accident"]:
+        if not validation_result.get("accident"):
             return create_response(400, {"error": "Image validation failed"})
 
         # Extract event details from the validation response
         event_details = validation_result.get("event_details")
 
         # Create the item to be stored in DynamoDB
-        item = create_dynamodb_item(item_id, body, image, event_details)
+        item = create_dynamodb_item(
+            item_id,
+            event_type,
+            event_timestamp,
+            latitude,
+            longitude,
+            image,
+            event_details,
+        )
 
         # Store the item in DynamoDB
         store_item_in_dynamodb(item)
@@ -69,8 +83,18 @@ def base64_encode_image(image):
     return ""
 
 
-def create_dynamodb_item(item_id, body, image, event_details):
-    return {"id": item_id, "body": body, "image": image, "event_details": event_details}
+def create_dynamodb_item(
+    item_id, event_type, event_timestamp, latitude, longitude, image, event_description
+):
+    return {
+        "id": item_id,
+        "event_type": event_type,
+        "event_timestamp": event_timestamp,
+        "latitude": latitude,
+        "longitude": longitude,
+        "image": image,
+        "event_description": event_description,
+    }
 
 
 def store_item_in_dynamodb(item):
