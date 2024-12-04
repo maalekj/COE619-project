b1552d39be340a7b018c2643084a4f4df6aff392
maalekj <maalekj@gmail.com>
2024-12-04 20:05:45 +0300

    update zipLambda after introducing the folder for libraries

--- zipLambda.sh
+++ zipLambda.sh
@@ -1,2 +1,7 @@
-zip record_event_lambda.zip record_event_lambda.py
-zip event_validate_lambda.zip event_validate_lambda.py
+#!/bin/bash
+
+# Zip the file
+zip -r record_event_lambda.zip record_event_lambda.py
+
+# Zip the contents of the event-evaluation folder
+zip -r event_validate_lambda.zip event_validate_lambda/
