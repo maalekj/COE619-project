24e4b96168dd173d84a4e12aa7aea72fe6804b00
maalekj <maalekj@gmail.com>
2024-12-06 15:43:34 +0300

    Update zipLambda.sh to include get_event_lambda function

--- zipLambda.sh
+++ zipLambda.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-# Zip the file
-zip -r record_event_lambda.zip record_event_lambda.py
+# Zip the files
+zip record_event_lambda.zip record_event_lambda.py
+zip get_event_lambda.zip get_event_lambda.py
 
 # Zip the contents of the event-evaluation folder
 zip -r event_validate_lambda.zip event_validate_lambda/
